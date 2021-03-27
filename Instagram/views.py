7bc2bd4ac6a2a96b2ce881b6c493d6e1e236feeb
seron-ux <abyseronkirui@gmail.com>
2021-03-28 01:33:43 +0300

    put a decorater login_url

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render(request,'index.html')
 

d5508f5d8d646bc8e6be13cef45c9bce5d7d850e
seron-ux <abyseronkirui@gmail.com>
2021-03-28 13:24:49 +0300

    form a comments function

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
 from .forms import RegistrationForm
 from django.contrib.auth.decorators import login_required
-from .forms import NewPostForm
-from .models import Image
+from .forms import NewPostForm,CommentForm
+from .models import Image,Comment
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -52,6 +52,28 @@ def newPost(request):
     else:
         form = NewPostForm()
     return render(request, 'newPost.html', {"form": form})
+
+
+
+
+@login_required(login_url='/accounts/login/')
+def comment(request,id):
+    comments = Comment.objects.filter(postt= id)
+    images = Image.objects.filter(id=id).all()
+    current_user = request.user
+    user_profile = Profile.objects.get(user = current_user)
+    image = get_object_or_404(Image, id=id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.postt = image
+            comment.userr = user_profile
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = CommentForm()
+    return render(request,'comment.html',{"form":form,"images":images,"comments":comments})
     
 
 

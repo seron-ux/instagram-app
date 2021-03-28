3258f0eca0cb4b9ab90802541ad1d32cf099779b
seron-ux <abyseronkirui@gmail.com>
2021-03-28 14:12:57 +0300

    made migrations for class comments

--- Instagram/models.py
+++ Instagram/models.py
@@ -6,11 +6,28 @@ class Image(models.Model):
     image = models.ImageField('images')
     imageName = models.CharField(max_length=30,blank=True)
     imageCaption = models.CharField(max_length=300)
+    comments = models.CharField(max_length=30,blank=True)
     # profile = models.ForeignKey(Profile,on_delete = models.CASCADE)
     # likes = models.ManyToManyField(User, related_name='likes', blank=True, )
-    # comments = models.CharField(max_length=30,blank=True)
+   
 
     def savePost(self):
         print(self)
         return self.save()
+
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    postt= models.ForeignKey(Image, on_delete=models.CASCADE)
+    # userr= models.ForeignKey(Profile, on_delete=models.CASCADE)
+    # created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+    def save_comment(self):
+        self.user
+
+    def delete_comment(self):
+        self.delete()
+
 

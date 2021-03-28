4b9da0c1886bb8c1f42c31677ac3cdf09e0c66ae
seron-ux <abyseronkirui@gmail.com>
2021-03-28 21:16:52 +0300

    import Instagram.signals

--- Instagram/apps.py
+++ Instagram/apps.py
@@ -1,5 +1,10 @@
+from __future__ import unicode_literals
+
 from django.apps import AppConfig
 
 
-class InstagramappConfig(AppConfig):
-    name = 'InstagramApp'
+class InstagramConfig(AppConfig):
+    name = 'Instagram'
+
+    def ready(self):
+        import Instagram.signals
